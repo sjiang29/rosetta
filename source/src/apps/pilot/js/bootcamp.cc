6c50fa7baaf8ef87974ebc124e24a9906a4545e4
sjiang29 <69825128+sjiang29@users.noreply.github.com>
2024-01-29 16:25:16 -0600

    add functionality to read a pdb file

--- source/src/apps/pilot/js/bootcamp.cc
+++ source/src/apps/pilot/js/bootcamp.cc
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <basic/options/option.hh>
+#include <basic/options/keys/in.OptionKeys.gen.hh>
+#include <devel/init.hh>
 
-int main() {
-    std::cout << "Hello World!!" << std::endl;
+int main(int argc, char ** argv) {
+
+    std::cout << "Hello World!" << std::endl;
+    devel::init( argc, argv );
+    utility::vector1< std::string > filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ].value();
+    if ( filenames.size() > 0 ) {
+        std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
+    } else {
+        std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
+        return 1;
+    }
+
     return 0;
 }
