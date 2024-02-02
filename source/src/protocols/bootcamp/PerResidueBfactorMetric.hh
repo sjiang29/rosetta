8a302e6b72e2191aea4a2bf2982bc1a1d350bc29
sjiang29 <69825128+sjiang29@users.noreply.github.com>
2024-02-02 10:45:58 -0600

    add needed files by using code template

---
+++ source/src/protocols/bootcamp/PerResidueBfactorMetric.hh
@@ -0,0 +1,132 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington CoMotion, email: license@uw.edu.
+
+/// @file protocols/bootcamp/PerResidueBfactorMetric.hh
+/// @brief create a subclass of PerResidueRealMetric class
+/// @author sjiang29 (69825128+sjiang29@users.noreply.github.com)
+
+#ifndef INCLUDED_protocols_bootcamp_PerResidueBfactorMetric_HH
+#define INCLUDED_protocols_bootcamp_PerResidueBfactorMetric_HH
+
+#include <protocols/bootcamp/PerResidueBfactorMetric.fwd.hh>
+#include <core/simple_metrics/PerResidueRealMetric.hh>
+
+// Core headers
+#include <core/types.hh>
+
+// Utility headers
+#include <utility/tag/XMLSchemaGeneration.fwd.hh>
+
+// C++ headers
+#include <map>
+
+#ifdef    SERIALIZATION
+// Cereal headers
+#include <cereal/types/polymorphic.fwd.hpp>
+#endif // SERIALIZATION
+
+namespace protocols {
+namespace bootcamp {
+
+///@brief create a subclass of PerResidueRealMetric class
+class PerResidueBfactorMetric : public core::simple_metrics::PerResidueRealMetric{
+
+public:
+
+	/////////////////////
+	/// Constructors  ///
+	/////////////////////
+
+	/// @brief Default constructor
+	PerResidueBfactorMetric();
+
+	/// @brief Destructor (important for properly forward-declaring smart-pointer members)
+	~PerResidueBfactorMetric() override;
+
+
+public:
+
+	/////////////////////
+	/// Metric Methods ///
+	/////////////////////
+
+	///Defined in RealMetric:
+	///
+	/// @brief Calculate the metric and add it to the pose as a score.
+	///           labeled as prefix+metric+suffix.
+	///
+	/// @details Score is added through setExtraScorePose and is output
+	///            into the score tables/file at pose output.
+	//void
+	//apply( pose::Pose & pose, prefix="", suffix="" ) override;
+
+
+	///Defined in PerResidueRealMetric
+	//void
+	//set_residue_selector( select::residue_selector::ResidueSelectorCOP selector );
+
+	///@brief Calculate the metric.
+	/// This map is Rosetta Resnum->value and includes only those residues selected.
+	///
+	///@details
+	/// Return by value as this function can not STORE the result, it only calculates.
+	/// Store the result in the pose by using the apply method, which calls this method and stores the result
+	/// in the pose as ExtraScoreValues.
+	std::map< core::Size, core::Real >
+	calculate( core::pose::Pose const & pose ) const override;
+
+public:
+
+	///@brief Name of the class
+	std::string
+	name() const override;
+
+	///@brief Name of the class for creator.
+	static
+	std::string
+	name_static();
+
+	///@brief Name of the metric
+	std::string
+	metric() const override;
+
+	/// @brief This simple metric is unpublished.  It returns sjiang29 as its author.
+	void provide_citation_info(basic::citation_manager::CitationCollectionList & ) const override;
+
+public:
+
+	/// @brief called by parse_my_tag -- should not be used directly
+	void
+	parse_my_tag(
+		utility::tag::TagCOP tag,
+		basic::datacache::DataMap & data ) override;
+
+	static
+	void
+	provide_xml_schema( utility::tag::XMLSchemaDefinition & xsd );
+
+	core::simple_metrics::SimpleMetricOP
+	clone() const override;
+
+#ifdef    SERIALIZATION
+public:
+	template< class Archive > void save( Archive & arc ) const;
+	template< class Archive > void load( Archive & arc );
+#endif // SERIALIZATION
+
+};
+
+} //bootcamp
+} //protocols
+
+#ifdef    SERIALIZATION
+CEREAL_FORCE_DYNAMIC_INIT( protocols_bootcamp_PerResidueBfactorMetric )
+#endif // SERIALIZATION
+
+#endif //protocols_bootcamp_PerResidueBfactorMetric_HH
---
+++ source/src/protocols/bootcamp/PerResidueBfactorMetricCreator.hh
@@ -0,0 +1,47 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington CoMotion, email: license@uw.edu.
+
+/// @file protocols/bootcamp/PerResidueBfactorMetricCreator.hh
+/// @brief create a subclass of PerResidueRealMetric class
+/// @author sjiang29 (69825128+sjiang29@users.noreply.github.com)
+
+#ifndef INCLUDED_protocols_bootcamp_PerResidueBfactorMetricCreator_HH
+#define INCLUDED_protocols_bootcamp_PerResidueBfactorMetricCreator_HH
+
+// Unit headers
+#include <core/simple_metrics/SimpleMetricCreator.hh>
+
+// Protocol headers
+#include <core/simple_metrics/SimpleMetric.fwd.hh>
+#include <utility/tag/XMLSchemaGeneration.fwd.hh>
+
+namespace protocols {
+namespace bootcamp {
+
+class PerResidueBfactorMetricCreator : public core::simple_metrics::SimpleMetricCreator {
+public:
+
+
+	/// @brief Instantiate a particular SimpleMetric
+	core::simple_metrics::SimpleMetricOP
+	create_simple_metric() const override;
+
+	/// @brief Return a string that will be used to instantiate the particular SimpleMetric
+	std::string
+	keyname() const override;
+
+	void
+	provide_xml_schema( utility::tag::XMLSchemaDefinition &) const override;
+};
+
+} //bootcamp
+} //protocols
+
+#endif //INCLUDED_protocols_bootcamp_PerResidueBfactorMetricCreator_HH
+
